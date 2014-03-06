cd9eb931950dc3cd86a3aca1cb6d02ae89c931d2
ajosepha <ariel.isaacs@gmail.com>
2014-03-06 11:56:05 -0500

    minor changes

--- app/models/instagram_datum.rb
+++ app/models/instagram_datum.rb
@@ -3,37 +3,37 @@ class InstagramDatum < ActiveRecord::Base
   attr_accessible :name, :username, :bio, :followers_count, :instagram_id, :location
   validates_uniqueness_of :username, scope: :user_id
 
-  # def create_with_instagram(current_user)
-  #   instagram_parser = InstagramParser.new(current_user)
-  #   data = instagram_parser.parse_and_sort
-  #   data.each do |datum|
-  #     d = InstagramDatum.new(datum)
-  #     d.user_id = current_user.id
-  #     d.save
+  def create_with_instagram(current_user)
+    instagram_parser = InstagramParser.new(current_user)
+    data = instagram_parser.parse_and_sort
+    data.each do |datum|
+      d = InstagramDatum.new(datum)
+      d.user_id = current_user.id
+      d.save
+    end
+  end
+
+
+  # def make_array
+  #   array = []
+  #   @test.each do |user|
+  #     array << {
+  #       :instagram_id => user["id"],
+  #       :username => user["username"],
+  #       :bio => user["bio"],
+  #       :name => user["full_name"]}
   #   end
+  #   array
   # end
 
-
-  def make_array
-    array = []
-    @test.each do |user|
-      array << {
-        :instagram_id => user["id"],
-        :username => user["username"],
-        :bio => user["bio"],
-        :name => user["full_name"]}
-    end
-    array
-  end
-
-  def self.save_dummy
-    data = make_array
-    array.each do |element|
-      dumb = InstagramDatum.new(element)
-      dumb.user_id = User.last.id
-      dumb.save
-    end
-  end
+  # def self.save_dummy
+  #   data = make_array
+  #   array.each do |element|
+  #     dumb = InstagramDatum.new(element)
+  #     dumb.user_id = User.last.id
+  #     dumb.save
+  #   end
+  # end
 
 end
 
--- app/models/instagram_parser.rb
+++ app/models/instagram_parser.rb
@@ -5,14 +5,15 @@ class InstagramParser
   def initialize(user_obj)
     @user_obj = user_obj
     Instagram.configure do |config|
-      config.client_id = "73c57238ee7a4998818a4bb43e3aa369"
-      config.client_secret = "3e9d04b20ad34cfea76b7a8fd5797308"
-      # config.client_id =  ENV['INSTAGRAM_ID']
-      # config.client_secret = ENV['INSTAGRAM_SECRET']
-      config.access_token = "225440768.2485542.3da25a35bab5486f831de5377915f66f"
-      # user_obj.authentications.find_by_provider("instagram").token
+      # config.client_id = "73c57238ee7a4998818a4bb43e3aa369"
+      # config.client_secret = "3e9d04b20ad34cfea76b7a8fd5797308"
+      config.client_id =  ENV['INSTAGRAM_ID']
+      config.client_secret = ENV['INSTAGRAM_SECRET']
+      # config.access_token = "225440768.2485542.3da25a35bab5486f831de5377915f66f"
+      user_obj.authentications.find_by_provider("instagram").token
     end
-    @test = Instagram.user_followed_by(9620227, :count => -1)
+    # @test = Instagram.user_followed_by(9620227, :count => -1)
+    @data = Instagram.user_followed_by(user_obj.authentications.find_by_provider("instagram").uid, :count => -1)
     @info_array = []
   end
 
@@ -33,8 +34,8 @@ class InstagramParser
         :username => user["username"],
         :bio => user["bio"],
         :name => user["full_name"],
-        :followers_count => user["counts"]["followed_by"]
-        #:location => get_location(id).first
+        :followers_count => user["counts"]["followed_by"],
+        :location => get_location(id).first
       }
   end
 
