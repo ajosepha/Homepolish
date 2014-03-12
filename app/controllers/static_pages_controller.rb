e2442a50cdb18f6a2bdf83168076bd0cbb354251
Armando Amador <Arm.amador@gmail.com>
2014-03-12 17:16:55 -0400

    Fix instagram current user data

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,11 +6,9 @@ class StaticPagesController < ApplicationController
     @twitteruser = current_user
     @twitter_parsed = @twitteruser.twitter_data.page(params[:page]).per(10)
     @loop = @twitter_parsed.length 
-    @instauser = current_user
-    instagram_followers = InstagramDatum.where("followers_count IS NOT NULL").order("followers_count DESC")
+    instauser = current_user
+    instagram_followers = instauser.instagram_data.where("followers_count IS NOT NULL").order("followers_count DESC")
     @instagram_parsed = instagram_followers.page(params[:page]).per(10)
-    #debugger
-    #puts 'hi'
   end
 
   def create
@@ -18,3 +16,5 @@ class StaticPagesController < ApplicationController
   end
 
 end
+
+
